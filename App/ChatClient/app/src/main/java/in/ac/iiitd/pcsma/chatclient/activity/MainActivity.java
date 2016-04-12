8378e98bb48597085472138ecaa40809b5791cd9
Shiv Kandikuppa <shiv12095@iiitd.ac.in>
2016-04-12 20:39:53 +0530

    Created Basic App with minimal UI

---
+++ App/ChatClient/app/src/main/java/in/ac/iiitd/pcsma/chatclient/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package in.ac.iiitd.pcsma.chatclient.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.ac.iiitd.pcsma.chatclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
