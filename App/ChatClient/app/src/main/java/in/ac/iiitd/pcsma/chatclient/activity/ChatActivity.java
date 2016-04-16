bcc328f48cc3ca94b3c8091f553ce0e9cbf1cf2c
Shiv Kandikuppa <shiv12095@iiitd.ac.in>
2016-04-16 17:55:32 +0530

    Added logout

--- App/ChatClient/app/src/main/java/in/ac/iiitd/pcsma/chatclient/activity/ChatActivity.java
+++ App/ChatClient/app/src/main/java/in/ac/iiitd/pcsma/chatclient/activity/ChatActivity.java
@@ -1,5 +1,7 @@
 package in.ac.iiitd.pcsma.chatclient.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +13,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -19,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import in.ac.iiitd.pcsma.chatclient.R;
+import in.ac.iiitd.pcsma.chatclient.commons.Constants;
 import in.ac.iiitd.pcsma.chatclient.fragment.GroupChatFragment;
 import in.ac.iiitd.pcsma.chatclient.fragment.PrivateChatFragment;
 
@@ -27,11 +32,14 @@ public class ChatActivity extends AppCompatActivity {
     private ViewPager viewPager;
     private TabLayout tabLayout;
     private Toolbar toolbar;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        sharedPreferences = getSharedPreferences(Constants.SHARED_PREFS, MODE_PRIVATE);
 
         toolbar = (Toolbar) findViewById(R.id.activity_chat_toolbar);
         setSupportActionBar(toolbar);
@@ -54,42 +62,6 @@ public class ChatActivity extends AppCompatActivity {
         });
 
     }
-
-
-//    /**
-//     * A placeholder fragment containing a simple view.
-//     */
-//    public static class PlaceholderFragment extends Fragment {
-//        /**
-//         * The fragment argument representing the section number for this
-//         * fragment.
-//         */
-//        private static final String ARG_SECTION_NUMBER = "section_number";
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        /**
-//         * Returns a new instance of this fragment for the given section
-//         * number.
-//         */
-//        public static PlaceholderFragment newInstance(int sectionNumber) {
-//            PlaceholderFragment fragment = new PlaceholderFragment();
-//            Bundle args = new Bundle();
-//            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-//            fragment.setArguments(args);
-//            return fragment;
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                                 Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment_chat, container, false);
-//            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-//            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-//            return rootView;
-//        }
-//    }
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
@@ -131,5 +103,25 @@ public class ChatActivity extends AppCompatActivity {
             mFragmentList.add(fragment);
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_chat, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_action_logout) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.remove(Constants.ACCESS_TOKEN);
+            editor.commit();
+            Intent intent = new Intent(ChatActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
